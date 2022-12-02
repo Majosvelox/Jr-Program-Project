49f594adf2867d922976a291d3227039fe3faf97
Majosvelox <119696580+Majosvelox@users.noreply.github.com>
2022-12-02 16:00:42 -0300

    Change Shape

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Color colorStart;
+    Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        // Event m_Event = Event.current;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ChangeColor();
+
+    }
+        public void ChangeColor() {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                //RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.name == "Cube")
+                    {
+                        colorStart = Color.blue;
+                        rend.material.color = colorStart;
+                        Debug.Log(" You change the color of the Shape, the new color is Blue. ");
+
+                    }
+
+                }
+            }
+        }
+    }
+
+    
+
